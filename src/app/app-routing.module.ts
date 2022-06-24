f2d2cf7b7acd0dadf96040a116bafa1947fb6d85
Jesus Edgar Duarte Caceres <jesus_edgar_duarte_caceres@hotmail.com>
2022-06-24 11:35:00 -0500

    firts commit template ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { DetalleComponent } from './ingresos-egresos/detalle/detalle.component';
+import { EstadisticaComponent } from './ingresos-egresos/estadistica/estadistica.component';
+import { IngresosEgresosComponent } from './ingresos-egresos/ingresos-egresos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: '', component: DashboardComponent,
+    children: [
+      { path: 'ingresos-egresos', component: IngresosEgresosComponent },
+      { path: 'detalle', component: DetalleComponent },
+      { path: '', component: EstadisticaComponent },
+      { path: '**', redirectTo: '' },
+    ]
+  },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { IngresosEgresosComponent } from './ingresos-egresos/ingresos-egresos.component';
+import { DetalleComponent } from './ingresos-egresos/detalle/detalle.component';
+import { EstadisticaComponent } from './ingresos-egresos/estadistica/estadistica.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent,
+    IngresosEgresosComponent,
+    DetalleComponent,
+    EstadisticaComponent,
   ],
   imports: [
     BrowserModule,
